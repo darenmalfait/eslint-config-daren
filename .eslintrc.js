30220b326068c8d18f6624d81ca73eb7da0385eb
Daren Malfait <github@daren.be>
2021-06-12 23:38:32 +0200

    fix: turn off prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -138,7 +138,7 @@ module.exports = {
     'react/prefer-es6-class': 'off',
     'react/prefer-read-only-props': 'off',
     'react/prefer-stateless-function': 'off',
-    'react/prop-types': 'error',
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'error',
     'react/require-default-props': 'off',
     'react/require-optimization': 'off',
