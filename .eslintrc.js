35bc577f269033bbc48d6332c848245af15f41b4
Daren Malfait <github@daren.be>
2021-10-05 14:09:49 +0200

    fix: turn off explicit-function-return-type

--- .eslintrc.js
+++ .eslintrc.js
@@ -264,19 +264,8 @@ module.exports = {
             ignoreRestSiblings: true,
           },
         ],
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowTypedFunctionExpressions: true,
-          },
-        ],
-        '@typescript-eslint/explicit-module-boundary-types': [
-          `warn`,
-          {
-            allowArgumentsExplicitlyTypedAsAny: true,
-          },
-        ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-empty-function': `off`,
         '@typescript-eslint/no-explicit-any': `off`,
         '@typescript-eslint/ban-types': [
